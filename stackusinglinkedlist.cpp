4c3023243cfa1341425ad9d92a2f5e45ea9c01b9
Daffansyah276 <daffansyaharya@gmail.com>
2025-05-22 10:46:01 +0700

    membuat class stack private

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -2,13 +2,69 @@
 using namespace std;
 
 // Node class representing a single node in the linked list
-class Node {
+class Node
+{
 public:
-int data;
-Node *next;
+    int data;
+    Node *next;
 
-Node() {
- next = NULL;
-
-}
+    Node()
+    {
+        next = NULL;
+    }
 };
+
+// Stack class
+class Stack
+{
+private:
+    Node *top; // Pointer to the top node of the stuck
+
+    Stack()
+    {
+        top = NULL; // Initialize the stack with a null top pointer
+    }
+
+    // Push operation : insert an element onto the top of the stuck
+    int push(int value)
+    {
+        Node *newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value;      // 2. assign value
+        newNode->next = top;        // 3. set the next pointer of the new node to the current top
+        top = newNode;              // 4. Update the top pointer to the new node
+        cout << " Push Value: " << value << endl;
+        return value;
+    }
+
+    // Pop operation : Remove the topmost element from the stack
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << " Stack is empty. " << endl;
+        }
+    
+        Node *temp = top; // create a temporary pointer to the top node
+        top = top->next;  // Update the top pointer to the next node
+        cout << " Popped value: " << top->data << endl;
+    }
+    // peek/top operation : retrive the value of the topmost element without removing
+    void peek(){
+        if (top == NULL)
+        {
+            cout << " List is empty. " << endl;
+        } 
+        else
+        {
+            Node* current = top;
+            while (current != NULL)
+            {
+                cout << current->data <<" " << endl;
+                current = current->next;
+            }
+            cout << endl;
+
+        }// Return the value of the top node
+    }
+    
+  
